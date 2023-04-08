705ffe330b267ce878e9aeae8d0d7c6efbff8966
Nati-keleme <124769535+nati-art@users.noreply.github.com>
2023-04-08 15:01:07 +0300

    Add files via upload
    A C++ program that accepts a positive integer, n, or a letter entered by the user and prints a pattern of butterfly.

---
+++ project.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n, m;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    m = 2*n + 1; // number of columns
+
+    // Upper half of the pattern
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= i; j++) {
+            if(j==n){
+                cout<<"";
+            }
+            else{
+            cout <<(i-j)%10; // print numbers in descending order
+            }
+        }
+        int c=m-2*i;
+        for (int j = 1; j < (c-1); j++) {
+
+                  cout <<" ";
+        }
+        for (int j = i; j >= 0; j--) {
+            cout <<(i-j)%10; // print numbers in ascending order
+        }
+        cout << endl;
+    }
+    // Lower half of the pattern
+    for (int i = n-1; i >= 0; i--) {
+        for (int j = 0; j <= i; j++) {
+            cout << (i-j)%10; // print numbers in descending order
+        }
+        int d=m-2*i;
+        for (int j = 0; j <(d-2) ; j++) {
+            if (j == i || j == m-i-1) {
+                cout << " ";
+            }
+            else {
+                cout << " "; // print spaces elsewhere
+            }
+        }
+        for (int j = i; j >= 0; j--) {
+            cout << (i-j)%10; // print numbers in ascending order
+        }
+        cout << endl;
+    }
+    return 0;
+}
