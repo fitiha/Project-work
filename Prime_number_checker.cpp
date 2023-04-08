c1939e74c1d63ce191e0722b55fc61b187b40305
Natnael-Bacha <111348356+Natnael-Bacha@users.noreply.github.com>
2023-04-08 15:25:41 +0300

    Update Prime_number_checker.cpp

--- Prime_number_checker.cpp
+++ Prime_number_checker.cpp
@@ -2,21 +2,21 @@
 #include <cmath>
 using namespace std;
 int main(){
-int j,i,y=0;
-float x =0;
-int remainder,k;
-int number,prime=1;
+float x = 0;
+int remainder,k,number,prime=1,y=0;
+ 
 Loop:
-cout<<" \n \n Enter any number: ";
+cout<<"Enter any number: ";
 cin>>number;
+ 
 //checks whether the number is prime or not.
- for(i=2; i<=sqrt(number); i++){
+ for(int i=2; i<=sqrt(number); i++){
     if(number%i==0){
         prime = 0;break;}}
 if(prime==1){
     cout<<number<<" is prime number"<<endl;
 
-    for(j=1; j<=number; j++){
+    for(int j=1; j<=number; j++){
             prime=1;
           for( int i=2; i<=sqrt(j); i++){
             if(j%i==0){
@@ -31,7 +31,7 @@ cout<<endl<<x/y<<" is their average";} //prints the average
 else {
     cout<<number<<" "<<"is not a prime number "<<endl;
           //finds the prime factors of the number
-          for(k=2; k<=sqrt(number); k++){
+          for(int k=2; k<=sqrt(number); k++){
                     prime=1;
              for( int i=2; i<=sqrt(k); i++){
                 if(k%i==0){
@@ -42,7 +42,9 @@ if(prime==1){
      cout<<k<<", ";
 }
   }  }
-cout<<" = prime factor  ";
-}goto Loop;
+cout<<" =  Are prime factors of "<<number;
+}
+ cout<<"\n \n";
+ goto Loop;
 return 0;
 }
