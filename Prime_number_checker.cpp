fdb38950b9234148fc8fdd5bf9135a87d8fb628f
Natnael Desalegn <natnaeljul7@gmail.com>
2023-04-09 22:08:48 +0300

    (update) _ prime number_checker_with_pointers

--- Prime_number_checker.cpp
+++ Prime_number_checker.cpp
@@ -1,50 +1,69 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
 int main(){
-float x = 0;
-int remainder,k,number,prime=1,y=0;
- 
-Loop:
-cout<<"Enter any number: ";
-cin>>number;
- 
-//checks whether the number is prime or not.
- for(int i=2; i<=sqrt(number); i++){
-    if(number%i==0){
-        prime = 0;break;}}
-if(prime==1){
-    cout<<number<<" is prime number"<<endl;
 
-    for(int j=1; j<=number; j++){
-            prime=1;
-          for( int i=2; i<=sqrt(j); i++){
-            if(j%i==0){
+    float x = 0;
+    int remainder, k, number, prime = 1, y = 0;
 
-            prime = 0;break;}
-          }
-if(prime == 1){
-x = x+j;
-y = y+1;
-cout<<j<<" ";}}cout<<" the  prime numbers before "<< number;
-cout<<endl<<x/y<<" is their average";} //prints the average
-else {
-    cout<<number<<" "<<"is not a prime number "<<endl;
-          //finds the prime factors of the number
-          for(int k=2; k<=sqrt(number); k++){
-                    prime=1;
-             for( int i=2; i<=sqrt(k); i++){
-                if(k%i==0){
-                prime = 0;break;}
-}
-if(prime==1){
-        if(number%k==0){
-     cout<<k<<", ";
-}
-  }  }
-cout<<" =  Are prime factors of "<<number;
-}
- cout<<"\n \n";
- goto Loop;
-return 0;
+    Loop:
+    cout<<"Enter any number: ";
+    cin>>number;
+
+    int *ptrPrime = &prime;
+    int *ptrY = &y;
+    float *ptrX = &x;
+    int *ptrK = &k;
+    int *ptrNumber = &number;
+
+    for(int i = 2; i <= sqrt(*ptrNumber); i++){
+        if(*ptrNumber % i == 0){
+            *ptrPrime = 0;
+            break;
+        }
+    }
+
+    if(*ptrPrime == 1){
+        cout<<*ptrNumber<<" is prime number"<<endl;
+        for(int j = 1; j <= *ptrNumber; j++){
+            *ptrPrime = 1;
+            for(int i = 2; i <= sqrt(j); i++){
+                if(j % i == 0){
+                    *ptrPrime = 0;
+                    break;
+                }
+            }
+            if(*ptrPrime == 1){
+                *ptrX = *ptrX + j;
+                *ptrY = *ptrY + 1;
+                cout<<j<<" ";
+            }
+        }
+        cout<<" the  prime numbers before "<< *ptrNumber;
+        cout<<endl<<*ptrX / (*ptrY)<<" is their average";
+    }
+    else {
+        cout<<*ptrNumber<<" "<<"is not a prime number "<<endl;
+        for(*ptrK = 2; *ptrK <= sqrt(*ptrNumber); (*ptrK)++){
+            *ptrPrime = 1;
+            for(int i = 2; i <= sqrt(*ptrK); i++){
+                if(*ptrK % i == 0){
+                    *ptrPrime = 0;
+                    break;
+                }
+            }
+            if(*ptrPrime == 1){
+                if(*ptrNumber % *ptrK == 0){
+                    cout<<*ptrK<<", ";
+                }
+            }
+        }
+        cout<<" =  Are prime factors of "<<*ptrNumber;
+    }
+
+    cout<<"\n \n";
+    goto Loop;
+
+    return 0;
 }
