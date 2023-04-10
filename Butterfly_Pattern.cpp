33463190df3984b9ebb7908d307c62d750c16823
arctictundra999 <130237428+arctictundra999@users.noreply.github.com>
2023-04-10 15:51:36 +0300

    Update Butterfly_Pattern.cpp
    Removed the cout at line 15 because it was unnecessary

--- Butterfly_Pattern.cpp
+++ Butterfly_Pattern.cpp
@@ -12,7 +12,7 @@ int main() {
     for (int i = 0; i <= n; i++) {
         for (int j = 0; j <= i; j++) {
             if(j==n){
-                cout<<"";
+                
             }
             else{
             cout <<(i-j)%10; // print numbers in descending order
